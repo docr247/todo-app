f0438769d16f630b2a284b3ba57deaafb0e141d0
docr <raviragas@gmail.com>
2023-04-28 11:09:00 +0800

    File Compressoe

---
+++ bonus16.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+label1 = sg.Text("Select files to compress: ")
+input1 = sg.Input()
+choose_btn1 = sg.FilesBrowse("Choose")
+
+label2 = sg.Text("Select destination folder: ")
+input2 = sg.Input()
+choose_btn2 = sg.FolderBrowse("Choose")
+
+compress_btn = sg.Button("Compress")
+window = sg.Window("File Compressor", layout=[[label1, input1, choose_btn1],
+                                              [label2, input2, choose_btn2],
+                                              [compress_btn]])
+while True:
+    event, values = window.read()
+    print(event, values)
+
+window.close()
