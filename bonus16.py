cbc1a7563b2692a0025b511ce050d90e08f35f8e
docr <raviragas@gmail.com>
2023-04-30 13:08:15 +0800

    Change the theme, add clock and handle errors

--- bonus16.py
+++ bonus16.py
@@ -1,12 +1,13 @@
 import PySimpleGUI as sg
+from zip_creator  import make_archive
 
 label1 = sg.Text("Select files to compress: ")
 input1 = sg.Input()
-choose_btn1 = sg.FilesBrowse("Choose")
+choose_btn1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Select destination folder: ")
 input2 = sg.Input()
-choose_btn2 = sg.FolderBrowse("Choose")
+choose_btn2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_btn = sg.Button("Compress")
 window = sg.Window("File Compressor", layout=[[label1, input1, choose_btn1],
@@ -15,5 +16,9 @@ window = sg.Window("File Compressor", layout=[[label1, input1, choose_btn1],
 while True:
     event, values = window.read()
     print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Compression completed!")
 
 window.close()
